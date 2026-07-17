35476f9c345bd2e6fee283536f61e9c50c8c8654
keerthana2005kk <sekarsekarcinnusami@gmail.com>
2026-07-17 15:17:35 +0530

    Add program to find the larger of two numbers

---
+++ Largestoftwonumbers.java
@@ -0,0 +1,13 @@
+public class Main
+{
+	public static void main(String[] args) {
+	   int a=2;
+	   int b=3;
+	   if (a>b){
+	       System.out.println("a is greater");
+	   }
+	   else{
+	       System.out.println("b is greater");
+	   }
+	   }
+	   }
